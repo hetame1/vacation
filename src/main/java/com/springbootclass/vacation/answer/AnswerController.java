5132629a38e88e5afd3c89a54ae284618d3354cd
hetame1 <poi8946@naver.com>
2024-11-15 11:16:55 +0900

    11/15

--- src/main/java/com/springbootclass/vacation/answer/AnswerController.java
+++ src/main/java/com/springbootclass/vacation/answer/AnswerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.springbootclass.vacation.question.Question;
 import com.springbootclass.vacation.question.QuestionService;
@@ -19,13 +20,19 @@ public class AnswerController {
   private final AnswerService answerService;
 
   @PostMapping("/create/{id}")
-  public String createAnswer(@PathVariable("id") Integer id, @RequestParam String content) {
+  public String createAnswer(@PathVariable("id") Integer id, @RequestParam(value = "content") String content) {
     /*
      * 답변을 저장, 저장하기 위해서는 이 답변이 참조하는 질문 객체가 필요
      * 답변 저장 전에 질문 객체를 불러와야 함
     */
     Question question = this.questionService.getQuestion(id);
     this.answerService.create(question, content);
+    return "redirect:/question/detail/" + id;
+  }
+
+  @GetMapping("/delete/{id}")
+  public String deleteAnswer(@PathVariable("id") Integer id) {
+    this.answerService.delete(id);
     return "redirect:/question/detail/" + id;
   }
 }
--- src/main/java/com/springbootclass/vacation/answer/AnswerService.java
+++ src/main/java/com/springbootclass/vacation/answer/AnswerService.java
@@ -19,11 +19,15 @@ public class AnswerService {
   */  
   public void create(Question question, String content) {
     Answer answer = new Answer();
-    
+
     answer.setContent(content);
     answer.setCreateDate(LocalDateTime.now());
     answer.setQuestion(question);
 
     this.answerRepository.save(answer);
+  }
+
+  public void delete(Integer id) {
+    this.answerRepository.deleteById(id);
   }
 }
